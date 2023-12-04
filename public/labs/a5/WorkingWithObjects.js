2971a9d65a8ed3ea89cbb924d31f5d20e36eeb67
Axel Boly <106812254+xrpq@users.noreply.github.com>
2023-12-04 07:15:59 -0500

    added alot

---
+++ public/labs/a5/WorkingWithObjects.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function WorkingWithObjects() {
+	const [assignment, setAssignment] = useState({
+		id: 1,
+		title: "NodeJS Assignment",
+		description: "Create a NodeJS server with ExpressJS",
+		due: "2021-10-10",
+		completed: false,
+		score: 0
+	});
+
+	const URL = "http://localhost:4000/a5/assignment";
+	const fetchAssignment = async () => {
+		const response = await axios.get(`${URL}`);
+		setAssignment(response.data);
+	};
+	const updateTitle = async () => {
+		const response = await axios.get(`${URL}/title/${assignment.title}`);
+		setAssignment(response.data);
+	};
+	useEffect(() => {
+		fetchAssignment();
+	}, []);
+
+	return (
+		<div>
+			<h3>Working With Objects</h3>
+			<h4>Modifying Properties</h4>
+			<a
+				target="_blank"
+				href={`${URL}/title/${assignment.title}`}
+				className="btn btn-primary me-2 float-end"
+				rel="noreferrer"
+			>
+				Update Title
+			</a>
+			<input
+				onChange={(e) =>
+					setAssignment({ ...assignment, title: e.target.value })
+				}
+				value={assignment.title}
+				className="form-control mb-2 w-75"
+				type="text"
+			/>
+			<button onClick={updateTitle} className="w-100 btn btn-primary mb-2">
+				Update Title to: {assignment.title}
+			</button>
+			<button onClick={fetchAssignment} className="w-100 btn btn-danger mb-2">
+				Fetch Assignment
+			</button>
+			<a
+				target="_blank"
+				href={`${URL}/score/${assignment.score}`}
+				className="btn btn-primary me-2 float-end"
+				rel="noreferrer"
+			>
+				Update Score
+			</a>
+			<input
+				onChange={(e) =>
+					setAssignment({ ...assignment, score: e.target.value })
+				}
+				value={assignment.score}
+				className="form-control mb-2 w-75"
+				type="number"
+			/>
+
+			<a
+				target="_blank"
+				href={`${URL}/completed/${assignment.completed}`}
+				className="btn btn-primary me-2 float-end"
+				rel="noreferrer"
+			>
+				Update Completed
+			</a>
+			<input
+				onChange={(e) => {
+					setAssignment({ ...assignment, completed: e.target.checked });
+				}}
+				checked={assignment.completed}
+				className="mb-2"
+				type="checkbox"
+				style={{ width: "25px", height: "25px" }}
+			/>
+			<h4>Retrieving Properties</h4>
+			<a
+				target="_blank"
+				href="http://localhost:4000/a5/assignment"
+				className="btn btn-primary me-2"
+				rel="noreferrer"
+			>
+				Get Assignment
+			</a>
+			<a
+				target="_blank"
+				href="http://localhost:4000/a5/assignment/title"
+				className="btn btn-primary me-2"
+				rel="noreferrer"
+			>
+				Get Title
+			</a>
+			<a
+				target="_blank"
+				href="http://localhost:4000/a5/assignment/score"
+				className="btn btn-primary me-2"
+				rel="noreferrer"
+			>
+				Get Score
+			</a>
+			<a
+				target="_blank"
+				href="http://localhost:4000/a5/assignment/completed"
+				className="btn btn-primary me-2"
+				rel="noreferrer"
+			>
+				Get Completed
+			</a>
+		</div>
+	);
+}
+export default WorkingWithObjects;
