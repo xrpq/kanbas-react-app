5685b9aeb971575a1f2b7c619a57cbdeb7188f27
Axel Boly <106812254+xrpq@users.noreply.github.com>
2023-12-18 19:47:37 -0500

    fixing merge issue

---
+++ src/Labs/a5/index.js
@@ -0,0 +1,21 @@
+import EncodingParametersInURLs from "./EncodingParametersInURLs";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithObjects from "./WorkingWithObjects";
+
+function Assignment5() {
+    return (
+      <div>
+        <h1>Assignment 5</h1>
+        <div className="list-group">
+          <a href="https://kanbas-node-server-app-3tck.onrender.coma5/welcome"
+             className="list-group-item">
+            Welcome
+          </a>
+          <EncodingParametersInURLs/>
+          <WorkingWithObjects/>
+          <WorkingWithArrays/>
+        </div>
+      </div>
+    );
+  }
+  export default Assignment5;
